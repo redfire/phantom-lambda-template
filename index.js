58fa6f133ae91fd7713d40c5fc9374430fe31851
BlackThunder <info@vasio.nl>
2017-01-25 19:16:24 +0100

    1.0 Test with all functionality

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-
-var AWS = require('aws-sdk');
 var phantomjs = require('phantomjs-prebuilt');
 
 exports.handler = function(event, context, callback) {
-    var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
+    var enabled_domains = [];
+    enabled_domains.push("goneout.nl");
+    enabled_domains.push("www.goneout.nl");
+    enabled_domains.push("coachgezocht.nu");
+    enabled_domains.push("www.coachgezocht.nu");
+
+    var url = event.url;
+    var domain = extractDomain(url);
+    var is_allowed = false;
+    for(var i in enabled_domains) {
+        if(enabled_domains[i] == domain) {
+            is_allowed = true;
+            break;
+        }
+    }
+    if(!is_allowed) {
+        callback(null, "Action not allowed");
+        return;
+    }
+
+    var phantom = phantomjs.exec('phantomjs-script.js', 'https://coachgezocht.nu');
     phantom.buffer = '';
     phantom.stdout.on('data', function(buf) {
-        this.buffer += String(buf);
-        console.log('[STR] stdout "%s"', String(buf));
+        phantom.buffer += String(buf);
+        //console.log('[STR] stdout "%s"', String(buf));
     });
     phantom.stderr.on('data', function(buf) {
-        console.log('[STR] stderr "%s"', String(buf));
+        //console.log('[STR] stderr "%s"', String(buf));
     });
     phantom.on('close', function(code) {
-        console.log('[END] code', code);
+        //console.log('[END] code', code);
     });
 
-    phantom.on('exit', code => {
+    phantom.on('exit', function(code) {
+        //console.log(this.buffer);
         callback(null, phantom.buffer);
     });
 
 };
+
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,2 +1,33 @@
-console.log('Boo from phantom');
-phantom.exit();
+var system = require('system');
+
+if (system.args.length < 2) {
+    console.log("Missing arguments.");
+    phantom.exit();
+}
+
+var page = require('webpage').create();
+page.settings.loadImages = false;
+page.settings.localToRemoteUrlAccessEnabled = true;
+page.settings.resourceTimeout = 15000;
+page.onCallback = function() {
+    //console.log('callback');
+    console.log(page.content);
+    page.close();
+    phantom.exit();
+};
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+    //console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+};
+page.onInitialized = function() {
+    //console.log('onInitialized');
+    page.evaluate(function() {
+        setTimeout(function() {
+            //console.log('calling home');
+            window.callPhantom();
+        }, 8500);
+    });
+};
+
+page.open(system.args[1], function(status) {
+    //console.log('status: ' + status);
+});
