ec0a094733cd904f77686f0ad8d266d8fb7e6097
BlackThunder <info@vasio.nl>
2017-01-25 18:22:23 +0100

    Test to get output of a buffer

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var AWS = require('aws-sdk');
 var phantomjs = require('phantomjs-prebuilt');
 
 exports.handler = function(event, context, callback) {
-    console.log('I am on master');
     var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
-
+    phantom.buffer = '';
     phantom.stdout.on('data', function(buf) {
+        this.buffer += String(buf);
         console.log('[STR] stdout "%s"', String(buf));
     });
     phantom.stderr.on('data', function(buf) {
@@ -17,7 +17,7 @@ exports.handler = function(event, context, callback) {
     });
 
     phantom.on('exit', code => {
-        callback(null, 'This is an integration test!!');
+        callback(null, phantom.buffer);
     });
 
 };
